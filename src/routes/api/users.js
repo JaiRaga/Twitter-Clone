6e38e27781a159a6624d01b323e868b1b2991b69
JaiRaga <raga.j.santhosh@gmail.com>
2020-07-11 19:31:49 +0530

    created routes for following and unfollowing users

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -17,6 +17,7 @@ router.post("/register", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
+    console.log(req.body);
     const user = await User.findByCredentials(
       req.body.email,
       req.body.password
@@ -28,12 +29,74 @@ router.post("/login", async (req, res) => {
   }
 });
 
+// Get my profile
 router.get("/user/me", auth, async (req, res) => {
   res.send(req.user);
 });
 
-// follow route
+// Get User Profile
+router.get("/user/:id", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).send("No User Found!");
+    res.send(user);
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+});
+
+// Logout user
+router.post("/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+
+    await req.user.save();
+    res.send();
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// follow a user
+router.post("/follow/:id", auth, async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+
+    if (!user) throw new Error();
+
+    user.followers.push({ userId: req.user._id });
+    req.user.following.push({ userId: user._id });
+
+    await user.save();
+    await req.user.save();
+
+    res.send("Success!");
+  } catch (err) {
+    res.status(400).send("Unable to follow user!");
+  }
+});
+
+// get number of followers
+router.get("/followers", auth, async (req, res) => {
+  try {
+    const followers = req.user.followers;
+    res.send(followers);
+  } catch (err) {
+    res.status(400).send("Unable to get Followers!");
+  }
+});
 
 // unfollow route
+router.post("/unfollow/:id", auth, async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    if (!user) throw new Error("User doesn't exist");
+    // req.user.following.filter()
+  } catch (err) {
+    res.status(400).send("Unable to Unfollow user!");
+  }
+});
 
 module.exports = router;
