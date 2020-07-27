391d92ca5e73220bfc193bf653751b87b45e8ea0
JaiRaga <raga.j.santhosh@gmail.com>
2020-07-27 15:07:45 +0530

    adding app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ require("./db/mongoose");
 const cors = require("cors");
 const usersRouter = require("./routes/api/users");
 const tweetsRouter = require("./routes/api/tweets");
+const commentRouter = require("./routes/api/comment");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", usersRouter);
 app.use("/api", tweetsRouter);
+app.use("/api", commentRouter);
 
 const port = process.env.PORT || 9000;
 
