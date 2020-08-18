d9c0e5eaa34def92a32bf44ed78aa9b4df06c0eb
JaiRaga <raga.j.santhosh@gmail.com>
2020-08-18 06:54:49 +0530

    added port

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ require("./db/mongoose");
 const cors = require("cors");
 const usersRouter = require("./routes/api/users");
 const tweetsRouter = require("./routes/api/tweets");
-const commentRouter = require("./routes/api/comment");
 
 const app = express();
 
@@ -11,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", usersRouter);
 app.use("/api", tweetsRouter);
-app.use("/api", commentRouter);
 
 const port = process.env.PORT || 9000;
 
