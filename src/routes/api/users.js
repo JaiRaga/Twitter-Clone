8e54897d4cb884a5313c31416a6475ccef22bc0f
JaiRaga <raga.j.santhosh@gmail.com>
2020-08-18 06:53:38 +0530

    removed console logs and created routes to get followers and update followers

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -17,7 +17,6 @@ router.post("/register", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
-    console.log(req.body);
     const user = await User.findByCredentials(
       req.body.email,
       req.body.password
@@ -45,6 +44,17 @@ router.get("/user/:id", auth, async (req, res) => {
   }
 });
 
+// Update User Profile
+router.patch("/user", auth, async (req, res) => {
+  try {
+    const { username, handle, email, avatar } = req.body;
+    const user = await req.user.updateUser(username, handle, email, avatar);
+    res.send(user);
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+});
+
 // Logout user
 router.post("/logout", auth, async (req, res) => {
   try {
@@ -64,27 +74,18 @@ router.post("/follow/:id", auth, async (req, res) => {
   try {
     const user = await User.findOne({ _id: req.params.id });
 
-    if (!user) throw new Error();
+    if (!user) throw new Error("User doesn't exist!");
 
     user.followers.push({ userId: req.user._id });
     req.user.following.push({ userId: user._id });
 
     await user.save();
     await req.user.save();
-
+    // console.log(req.user.following);
+    // res.send(req.user.following);
     res.send("Success!");
   } catch (err) {
     res.status(400).send("Unable to follow user!");
-  }
-});
-
-// get number of followers
-router.get("/followers", auth, async (req, res) => {
-  try {
-    const followers = req.user.followers;
-    res.send(followers);
-  } catch (err) {
-    res.status(400).send("Unable to get Followers!");
   }
 });
 
@@ -92,10 +93,60 @@ router.get("/followers", auth, async (req, res) => {
 router.post("/unfollow/:id", auth, async (req, res) => {
   try {
     const user = await User.findOne({ _id: req.params.id });
+
     if (!user) throw new Error("User doesn't exist");
-    // req.user.following.filter()
+
+    user.followers = user.followers.filter(
+      (follower) => follower.userId.toString() !== req.user._id.toString()
+    );
+
+    req.user.following = req.user.following.filter(
+      (follow) => follow.userId.toString() !== req.params.id
+    );
+
+    await user.save();
+    await req.user.save();
+    // console.log(req.user.following);
+    // res.send(req.user.following);
+    res.send("Success!");
   } catch (err) {
     res.status(400).send("Unable to Unfollow user!");
+  }
+});
+
+// get number of followers
+router.get("/followers", auth, async (req, res) => {
+  try {
+    const followers = req.user.followers;
+    Promise.all(
+      followers.map(async (follower) => {
+        let user = await User.findById(follower.userId);
+        if (user) return user;
+      })
+    )
+      .then((result) => res.send(result))
+      .catch((err) => res.send(err));
+    // res.send(followers);
+  } catch (err) {
+    res.status(400).send("Unable to get Followers!");
+  }
+});
+
+// get number of following users
+router.get("/following", auth, async (req, res) => {
+  try {
+    const following = req.user.following;
+    Promise.all(
+      following.map(async (follow) => {
+        let user = await User.findById(follow.userId);
+        if (user) return user;
+      })
+    )
+      .then((result) => res.send(result))
+      .catch((err) => res.send(err));
+    // res.send(followers);
+  } catch (err) {
+    res.status(400).send("Unable to get Followers!");
   }
 });
 
