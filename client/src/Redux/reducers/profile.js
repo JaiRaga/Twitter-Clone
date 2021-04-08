3ce6a3184dc6410e222fd14abeaaaeae83f311dd
JaiRaga <raga.j.santhosh@gmail.com>
2021-04-08 16:37:03 +0530

    added new state property to hold user details

--- client/src/Redux/reducers/profile.js
+++ client/src/Redux/reducers/profile.js
@@ -1,46 +1,55 @@
 import {
-  GET_PROFILE,
-  CLEAR_PROFILE,
-  UPDATE_PROFILE,
-  GET_FOLLOWERS,
-  GET_FOLLOWING,
-  UPDATE_FOLLOWING
-} from "../actions/types";
+	GET_PROFILE,
+	CLEAR_PROFILE,
+	UPDATE_PROFILE,
+	GET_FOLLOWERS,
+	GET_FOLLOWING,
+	UPDATE_FOLLOWING,
+	GET_USERS,
+} from '../actions/types'
 
 const initialState = {
-  profile: null,
-  followers: [],
-  following: [],
-  loading: true,
-  error: {}
-};
+	profile: null,
+	people: [],
+	followers: [],
+	following: [],
+	loading: true,
+	error: {},
+}
 
 export default (state = initialState, action) => {
-  const { type, payload } = action;
+	const { type, payload } = action
 
-  switch (type) {
-    case GET_FOLLOWERS:
-      return {
-        ...state,
-        followers: [...payload],
-        loading: false
-      };
+	switch (type) {
+		case GET_USERS:
+			return {
+				...state,
+				people: [...payload],
+				loading: false,
+			}
 
-    case GET_FOLLOWING:
-      return {
-        ...state,
-        following: [...payload],
-        loading: false
-      };
+		case GET_FOLLOWERS:
+			return {
+				...state,
+				followers: [...payload],
+				loading: false,
+			}
 
-    case CLEAR_PROFILE:
-      return {
-        ...state,
-        profile: null,
-        loading: false
-      };
+		case GET_FOLLOWING:
+			return {
+				...state,
+				following: [...payload],
+				loading: false,
+			}
 
-    default:
-      return state;
-  }
-};
+		case CLEAR_PROFILE:
+			return {
+				...state,
+				profile: null,
+				loading: false,
+			}
+
+		default:
+			return state
+	}
+}
