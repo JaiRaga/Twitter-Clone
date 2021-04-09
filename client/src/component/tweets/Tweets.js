634dbd1f6a7f1eac5fd2999e4b76d84a74aa1c72
JaiRaga <raga.j.santhosh@gmail.com>
2021-04-09 11:47:15 +0530

    trying to display no tweets msg if tweets prop is empty

--- client/src/component/tweets/Tweets.js
+++ client/src/component/tweets/Tweets.js
@@ -1,16 +1,27 @@
-import React from "react";
-import { Grid } from "@material-ui/core";
+import React from 'react'
+import { Grid, makeStyles, Typography } from '@material-ui/core'
 
-import TweetItem from "./TweetItem";
+import TweetItem from './TweetItem'
+
+const useStyles = makeStyles((theme) => ({
+	display: {
+		display: 'flex',
+		flexWrap: 'nowrap',
+	},
+}))
 
 const Tweets = ({ tweets }) => {
-  return (
-    <Grid container item>
-      {tweets.map((tweet) => (
-        <TweetItem key={tweet._id} tweet={tweet} />
-      ))}
-    </Grid>
-  );
-};
+	const classes = useStyles()
+	return (
+		<Grid container item>
+			{tweets.length === 0
+				? // <Grid container item direction='column'>
+				  // 	<h3 className={classes.display}>No Tweets Found.</h3>
+				  // </Grid>
+				  null
+				: tweets.map((tweet) => <TweetItem key={tweet._id} tweet={tweet} />)}
+		</Grid>
+	)
+}
 
-export default Tweets;
+export default Tweets
