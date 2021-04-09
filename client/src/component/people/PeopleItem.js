02251e6292877bc3c5a2090b2c85511a00e788c6
JaiRaga <raga.j.santhosh@gmail.com>
2021-04-09 10:35:09 +0530

    created func to handle clicks and dispatch to store user in store

--- client/src/component/people/PeopleItem.js
+++ client/src/component/people/PeopleItem.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Paper, Grid, Avatar, Typography, Button } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router'
 import SideProfile from '../profile/SideProfile'
+import { setProfile } from '../../Redux/actions/profile'
 
 const PeopleItem = ({ user }) => {
+	const dispatch = useDispatch()
 	const history = useHistory()
 	const { _id, name, handle, followers, following } = user
+
+	const handleClick = () => {
+		dispatch(setProfile(user))
+		history.push(`/people/${_id}`)
+	}
+
 	return (
 		<Paper elevation={11}>
-			<Button onClick={() => history.push(`/people/${_id}`)}>
+			<Button onClick={handleClick}>
 				<Grid container item direction='column' spacing={2}>
 					<Grid item>
 						<Grid container item spacing={1}>
